f76e2c5daa12685f48189e969f65910d64b1bc6b
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-06-06 12:11:49 +0530

    Completled Intents Concept

--- MyIntents/app/src/main/java/com/muneiah/myintents/MainActivity.java
+++ MyIntents/app/src/main/java/com/muneiah/myintents/MainActivity.java
@@ -9,17 +9,21 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-EditText myinput_url;
+EditText myinput_url,et_ourMessge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myinput_url=findViewById(R.id.inputUrl);
+        et_ourMessge=findViewById(R.id.et_msg);
     }
     //With in the app screen we are navigating second screen
     //explicit intent
     public void navigate_secondScreen(View view) {
+        String mymessge_input=et_ourMessge.getText().toString();
         Intent myintentObj=new Intent(this,SecondActivity.class);
+        //By using this intentobj.putExtra() it will be sore the data as key and value pair
+        myintentObj.putExtra("apssdc",mymessge_input);
         startActivity(myintentObj);
     }
 //Implicit Intent
--- MyIntents/app/src/main/java/com/muneiah/myintents/SecondActivity.java
+++ MyIntents/app/src/main/java/com/muneiah/myintents/SecondActivity.java
@@ -2,13 +2,21 @@ package com.muneiah.myintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tv=findViewById(R.id.tv_mymessage_data);
+        Intent myintent=getIntent();
+        String data=myintent.getStringExtra("apssdc");
+        tv.setText(data);
     }
 }
+/*create a new project .in that project take one edittext and button .suppose we enter the mobile number as input open th dail pad and show that mobile number on dail pad  */
+/*Refer the google slides 2.1*/
