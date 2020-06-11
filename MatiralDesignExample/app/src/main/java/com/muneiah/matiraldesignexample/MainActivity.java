707045205abc8c54f3fa1786c18f66268986c6cc
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-06-11 13:12:52 +0530

    Day -10(11-06-2020) Material Design concept

---
+++ MatiralDesignExample/app/src/main/java/com/muneiah/matiraldesignexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.muneiah.matiraldesignexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonEvents(View view) {
+    }
+}
