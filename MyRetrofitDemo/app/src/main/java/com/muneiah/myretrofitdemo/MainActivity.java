e87cd4958c073cc0207eaedf65852161495c7050
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-06-17 10:14:18 +0530

    Retrofit example added

--- MyRetrofitDemo/app/src/main/java/com/muneiah/myretrofitdemo/MainActivity.java
+++ MyRetrofitDemo/app/src/main/java/com/muneiah/myretrofitdemo/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -23,24 +24,29 @@ MyPhotosAdapter adapter;
         setContentView(R.layout.activity_main);
         rv=findViewById(R.id.myrecyerview);
         progrss=new ProgressDialog(this);
+        progrss.setTitle("Fetching Data From Internet");
+        progrss.setMessage("please wait ..Loading..");
+        progrss.show();
         EndPointDataInterface service=RetrofitInstance.getJsonResponds().create(EndPointDataInterface.class);
         Call<List<Response>> call=service.getAllPhotos();
         call.enqueue(new Callback<List<Response>>() {
             @Override
             public void onResponse(Call<List<Response>> call, retrofit2.Response<List<Response>> response) {
                 progrss.dismiss();
-                getDataService(response.body());
+                getDataResponds(response.body());
+                Toast.makeText(MainActivity.this, "Success ", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailure(Call<List<Response>> call, Throwable t) {
-
+                Toast.makeText(MainActivity.this, "Somthing went wrong while fetching data..", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void getDataService(List<Response> body) {
+    private void getDataResponds(List<Response> body) {
         adapter=new MyPhotosAdapter(this,body);
+        Toast.makeText(this, ""+body.size(), Toast.LENGTH_SHORT).show();
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(adapter);
     }
